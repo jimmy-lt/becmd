ccdc18ac2947adcfc5b372a1b3fd2df98ff8edfa
Jimmy Thrasibule <jimmy@thrasibule.mx>
2018-12-15 00:30:18 +0100

    b/errors: Define a base exception class for becmd
    Define the :class:`becmd.errors.BaseError` class as a base class for
    all exceptions raised by the ``becmd`` program.
    
    Signed-off-by: Jimmy Thrasibule <jimmy@thrasibule.mx>

---
+++ becmd/errors.py
@@ -0,0 +1,24 @@
+# becmd/errors.py
+# ===============
+#
+# Copying
+# -------
+#
+# Copyright (c) 2018 becmd authors.
+#
+# This file is part of the *becmd* project.
+#
+# becmd is a free software project. You can redistribute it and/or
+# modify if under the terms of the MIT License.
+#
+# This software project is distributed *as is*, WITHOUT WARRANTY OF ANY
+# KIND; including but not limited to the WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE and NONINFRINGEMENT.
+#
+# You should have received a copy of the MIT License along with becmd.
+# If not, see <http://opensource.org/licenses/MIT>.
+#
+
+
+class BaseError(Exception):
+    """Base class for all exception raised by ``becmd``."""
